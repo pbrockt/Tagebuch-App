4fbd5adb2e6d5143f86afd9c1afe9de10bc62181
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 16:25:45 +0200

    Fix WebDAV crash + test button: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pbrockt.tagebuch"
         minSdk = 35
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1a"
+        versionCode = 3
+        versionName = "0.1b"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
