589628bdd51bd201e497d269900d66b8afd57c1e
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 16:25:41 +0200

    Fix WebDAV crash + test button: SyncRepository.kt

--- app/src/main/java/com/pbrockt/tagebuch/data/repository/SyncRepository.kt
+++ app/src/main/java/com/pbrockt/tagebuch/data/repository/SyncRepository.kt
@@ -13,8 +13,16 @@ class SyncRepository @Inject constructor(private val syncManager: SyncManager) {
     private val _syncState = MutableStateFlow<SyncResult?>(null)
     val syncState: StateFlow<SyncResult?> = _syncState
 
+    private val _testState = MutableStateFlow<SyncResult?>(null)
+    val testState: StateFlow<SyncResult?> = _testState
+
     suspend fun triggerSync() {
         _syncState.value = null
         _syncState.value = syncManager.sync()
+    }
+
+    suspend fun testConnection(url: String, user: String, pass: String) {
+        _testState.value = null
+        _testState.value = syncManager.testConnection(url, user, pass)
     }
 }
