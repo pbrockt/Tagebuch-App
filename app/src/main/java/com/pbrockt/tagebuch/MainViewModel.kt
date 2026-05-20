d7fdd8a7e6aa11a6c46e35bd2d7762cdd8007a92
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 08:30:09 +0200

    Update MainViewModel.kt

---
+++ app/src/main/java/com/pbrockt/tagebuch/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.pbrockt.tagebuch
+
+import androidx.lifecycle.ViewModel
+import com.pbrockt.tagebuch.data.local.prefs.SecurePrefs
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val prefs: SecurePrefs) : ViewModel() {
+
+    private val _themeChoice = MutableStateFlow(prefs.themeChoice)
+    val themeChoice: StateFlow<String> = _themeChoice
+
+    private val _accentColor = MutableStateFlow(prefs.accentColor)
+    val accentColor: StateFlow<String> = _accentColor
+
+    fun refresh() {
+        _themeChoice.value = prefs.themeChoice
+        _accentColor.value = prefs.accentColor
+    }
+}
