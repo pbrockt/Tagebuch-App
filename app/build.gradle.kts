704a7ecd2ed945572a821809bd4539eacc49253a
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 17:31:06 +0200

    Fix mood icon in calendar: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pbrockt.tagebuch"
         minSdk = 35
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1d"
+        versionCode = 6
+        versionName = "0.1e"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
