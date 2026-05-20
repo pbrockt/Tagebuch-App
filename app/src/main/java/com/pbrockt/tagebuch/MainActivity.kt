48bb8ed013c77381895ca0331a223d8c95c19cb8
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 08:30:10 +0200

    Update MainActivity.kt

--- app/src/main/java/com/pbrockt/tagebuch/MainActivity.kt
+++ app/src/main/java/com/pbrockt/tagebuch/MainActivity.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.pbrockt.tagebuch.navigation.AppNavigation
 import com.pbrockt.tagebuch.ui.theme.TagebuchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TagebuchTheme {
-                AppNavigation()
+            val themeChoice by mainViewModel.themeChoice.collectAsState()
+            val accentColor by mainViewModel.accentColor.collectAsState()
+
+            TagebuchTheme(themeChoice = themeChoice, accentColor = accentColor) {
+                AppNavigation(onThemeChanged = { mainViewModel.refresh() })
             }
         }
     }
