2fb741e8223c6c8f0931022032cdb645203ba389
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 09:25:17 +0200

    Update TagebuchApplication.kt

--- app/src/main/java/com/pbrockt/tagebuch/TagebuchApplication.kt
+++ app/src/main/java/com/pbrockt/tagebuch/TagebuchApplication.kt
@@ -1,7 +1,13 @@
 package com.pbrockt.tagebuch
 
 import android.app.Application
+import com.pbrockt.tagebuch.notifications.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TagebuchApplication : Application()
+class TagebuchApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        NotificationHelper.createChannel(this)
+    }
+}
