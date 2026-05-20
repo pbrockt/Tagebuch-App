4751b719428723c9d3561b96f0faa6456fec8e09
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-20 17:15:44 +0200

    Fix calendarIconMode refresh: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pbrockt.tagebuch"
         minSdk = 35
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1c"
+        versionCode = 5
+        versionName = "0.1d"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
