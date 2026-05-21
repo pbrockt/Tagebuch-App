dc8c7a635d8edd68c7be76ccd9e66cf749265abd
pbrockt <phillipp.brosch@aus-hesel.de>
2026-05-21 11:27:22 +0200

    v0.3a: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pbrockt.tagebuch"
         minSdk = 35
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.3"
+        versionCode = 11
+        versionName = "0.3a"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
